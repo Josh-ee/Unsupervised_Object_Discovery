af4096253bb1d6ac49451189314b05f86bbf3e21
Josh-ee <74482044+Josh-ee@users.noreply.github.com>
2023-12-16 01:25:09 -0600

    improved comments on tell_story

--- src/llm_integration/tell_story.py
+++ src/llm_integration/tell_story.py
@@ -1,21 +1,17 @@
-#import openai
-import re
-# from pathlib import Path
-
-
 from openai import OpenAI
 
 # Use your OpenAI key (starts with 'sk-')
 client = OpenAI(api_key ='sk-')
 
 with open('src/llm_integration/ros_log_latest_run.txt', 'r') as file:
-    # Read the entire content of the file into a single string
+    # Read the entire content of the file into a single string that includes the new lines
     log_text = file.read()
 
-# The variable file_contents now holds the entire content of the file as a string
+
 print(log_text)
 
-prompt = f"""You are a quadruped robot tasked with finding purple cups in the environment, testing if they are movable, and then bringing movable cups to the goal.
+# The log_text then gets input the story_prompt below
+story_prompt = f"""You are a quadruped robot tasked with finding purple cups in the environment, testing if they are movable, and then bringing movable cups to the goal.
     Tell a short story from first person perspective of how you navigated the environment based on the provided context.
     The way you test a cup is movable is by crouching down and seeing if it sticks to your velcro beard
     We do not know if the cup is movable until you attempt to pick it up.
@@ -31,11 +27,11 @@ completion = client.chat.completions.create(
     model='gpt-4',
     messages = [{
         'role': 'user',
-        'content': prompt
+        'content': story_prompt
     }],
-    temperature=0.1,
+    temperature=0.1, # Low temperature means less creative
     top_p=0.1,
-    max_tokens=1500,
+    max_tokens=1500, # Can increase for longer missions
 )
 
 print(completion.choices[0].message.content)
@@ -43,9 +39,10 @@ print(completion.choices[0].message.content)
 print("\n\nConverting to audio...")
 response = client.audio.speech.create(
   model="tts-1",
-  voice="echo",
+  voice="echo", # Choose voice here
   input=completion.choices[0].message.content
 )
 
+# Save to file
 response.stream_to_file('src/llm_integration/mission_story.mp3')
 print('done')
